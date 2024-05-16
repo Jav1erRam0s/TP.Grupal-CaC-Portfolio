867b378fa8ec3c1d8ebe0b65928eedf28209817b
Carolina <crinaldi@frba.utn.edu.ar>
2024-05-16 20:49:35 -0300

    agrego funcion para validar form

--- js/script.js
+++ js/script.js
@@ -2,3 +2,40 @@
 
 var today = new Date();
 document.getElementById("anio-footer").innerHTML = today.getFullYear();
+
+
+// VALIDATION FORM
+
+function validateForm(event) {
+    event.preventDefault();
+  
+    //inputs
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+  
+    if (nombre.trim() === "") {
+      alert("por favor ingresa un nombre");
+      return false; // EVITA QUE SE ENVIE EL FORMULARIo
+    }
+  
+    if (email.trim() === "") {
+      alert("por favor ingresa un email");
+      return false;
+    }
+  
+    if (!isValidEmail(email)) {
+      alert("Por favor ingresa un MAIL VALIDO");
+      return true;
+    }
+  
+    function isValidEmail(email) {
+      let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+      return emailRegex.test(email);
+    }
+  
+    alert("Formulario enviado correctamente");
+    return true;
+  }
+  
+  document.getElementById("contactForm").addEventListener("submit", validateForm);
